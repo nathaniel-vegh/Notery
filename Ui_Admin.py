ddd9bfecf144007c5113c00edeef47af96e36c58
Nate <fjw18228093247@gmail.com>
2019-01-19 11:14:10 -0600

    gitignore

--- Ui_Admin.py
+++ Ui_Admin.py
@@ -7,31 +7,13 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
-from BookStorage import BookStorageViewer
 
-<<<<<<< HEAD
-class Ui_Admin(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setupUi()
-        
-    def setupUi(self):
-        self.setObjectName("self")
-        self.resize(950, 580)
-        self.centralWidget = QtWidgets.QWidget(self)
-=======
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        font = QFont()
-        font.setFamily("Candara")
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(973, 582)
         MainWindow.setIconSize(QtCore.QSize(50, 50))
         self.centralWidget = QtWidgets.QWidget(MainWindow)
->>>>>>> parent of 294faff... Finished the student management page and updated the admin page
         self.centralWidget.setObjectName("centralWidget")
         self.verticalLayoutWidget = QtWidgets.QWidget(self.centralWidget)
         self.verticalLayoutWidget.setGeometry(QtCore.QRect(10, 10, 951, 491))
@@ -39,57 +21,47 @@ class Ui_MainWindow(object):
         self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout.setObjectName("verticalLayout")
-<<<<<<< HEAD
-        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
-        self.setCentralWidget(self.centralWidget)
-        self.storageView = BookStorageViewer()
-        self.verticalLayout.addWidget(self.storageView)
-        self.toolBar = QtWidgets.QToolBar(self)
-=======
-        self.storageView = BookStorageViewer()
-        self.verticalLayout.addWidget(self.storageView)
         MainWindow.setCentralWidget(self.centralWidget)
         self.toolBar = QtWidgets.QToolBar(MainWindow)
->>>>>>> parent of 294faff... Finished the student management page and updated the admin page
         self.toolBar.setIconSize(QtCore.QSize(60, 60))
         self.toolBar.setObjectName("toolBar")
-        self.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
-        self.action_add_book = QtWidgets.QAction(self)
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
+        self.action_add_book = QtWidgets.QAction(MainWindow)
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(":/rsc/rsc/manage_ebooks_button.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.action_add_book.setIcon(icon)
         self.action_add_book.setObjectName("action_add_book")
-        self.action_manage_students = QtWidgets.QAction(self)
+        self.action_manage_students = QtWidgets.QAction(MainWindow)
         icon1 = QtGui.QIcon()
         icon1.addPixmap(QtGui.QPixmap(":/rsc/rsc/manage_students_button.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.action_manage_students.setIcon(icon1)
         self.action_manage_students.setObjectName("action_manage_students")
-        self.action_report = QtWidgets.QAction(self)
+        self.action_report = QtWidgets.QAction(MainWindow)
         icon2 = QtGui.QIcon()
         icon2.addPixmap(QtGui.QPixmap(":/rsc/rsc/report.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.action_report.setIcon(icon2)
         self.action_report.setObjectName("action_report")
-        self.action_delete = QtWidgets.QAction(self)
+        self.action_delete = QtWidgets.QAction(MainWindow)
         icon3 = QtGui.QIcon()
         icon3.addPixmap(QtGui.QPixmap(":/rsc/rsc/trash_button.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.action_delete.setIcon(icon3)
         self.action_delete.setObjectName("action_delete")
-        self.action_star = QtWidgets.QAction(self)
+        self.action_star = QtWidgets.QAction(MainWindow)
         icon4 = QtGui.QIcon()
         icon4.addPixmap(QtGui.QPixmap(":/rsc/rsc/star_button.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.action_star.setIcon(icon4)
         self.action_star.setObjectName("action_star")
-        self.action_settings = QtWidgets.QAction(self)
+        self.action_settings = QtWidgets.QAction(MainWindow)
         icon5 = QtGui.QIcon()
         icon5.addPixmap(QtGui.QPixmap(":/rsc/rsc/settings_button.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.action_settings.setIcon(icon5)
         self.action_settings.setObjectName("action_settings")
-        self.action_add_record = QtWidgets.QAction(self)
+        self.action_add_record = QtWidgets.QAction(MainWindow)
         icon6 = QtGui.QIcon()
         icon6.addPixmap(QtGui.QPixmap(":/rsc/rsc/add_record_button.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.action_add_record.setIcon(icon6)
         self.action_add_record.setObjectName("action_add_record")
-        self.action_edit_record = QtWidgets.QAction(self)
+        self.action_edit_record = QtWidgets.QAction(MainWindow)
         icon7 = QtGui.QIcon()
         icon7.addPixmap(QtGui.QPixmap(":/rsc/rsc/edit_button.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.action_edit_record.setIcon(icon7)
@@ -102,39 +74,13 @@ class Ui_MainWindow(object):
         self.toolBar.addAction(self.action_report)
         self.toolBar.addAction(self.action_settings)
         self.toolBar.addAction(self.action_delete)
-<<<<<<< HEAD
-        #self.toolBar.actionTriggered[self.action_add_book].connect(self.AddBookButtonClicked)
-
-
-        self.retranslateUi(self)
-        QtCore.QMetaObject.connectSlotsByName(self)
-        
-    def addBookButtonClicked(self):
-        addDialog = addBookDialog(self)
-        addDialog.add_book_success_signal.connect(self.storageView.searchButtonClicked)
-        addDialog.show()
-        addDialog.exec_()
-=======
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
->>>>>>> parent of 294faff... Finished the student management page and updated the admin page
 
-    def retranslateUi(self):
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-<<<<<<< HEAD
-        self.setWindowTitle(_translate("self", "Notery"))
-        self.toolBar.setWindowTitle(_translate("self", "toolBar"))
-        self.action_add_book.setText(_translate("self", "Add Book"))
-        self.action_manage_students.setText(_translate("self", "Manage Students"))
-        self.action_report.setText(_translate("self", "Report"))
-        self.action_delete.setText(_translate("self", "Delete"))
-        self.action_star.setText(_translate("self", "Star"))
-        self.action_settings.setText(_translate("self", "Settings"))
-        self.action_add_record.setText(_translate("self", "Add Record"))
-        self.action_edit_record.setText(_translate("self", "Edit Record"))
-=======
-        MainWindow.setWindowTitle(_translate("Notery", "Notery"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
         self.action_add_book.setText(_translate("MainWindow", "Add Book"))
         self.action_manage_students.setText(_translate("MainWindow", "Manage Students"))
@@ -144,23 +90,15 @@ class Ui_MainWindow(object):
         self.action_settings.setText(_translate("MainWindow", "Settings"))
         self.action_add_record.setText(_translate("MainWindow", "Add Record"))
         self.action_edit_record.setText(_translate("MainWindow", "Edit Record"))
->>>>>>> parent of 294faff... Finished the student management page and updated the admin page
 
 import resources_rc
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-<<<<<<< HEAD
-    app.setWindowIcon(QIcon("./rsc/ereader.png"))
-    mainWindow = Ui_Admin()
-    mainWindow.show()
-=======
-    app.setWindowIcon(QIcon('./rsc/ereader.png'))
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
->>>>>>> parent of 294faff... Finished the student management page and updated the admin page
     sys.exit(app.exec_())
 
